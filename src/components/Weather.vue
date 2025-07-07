8a9b6bdeb468bdfafae15efe6ca4f1543f626a76
Romain Meunier <meunier.rm@gmail.com>
2025-07-07 22:40:55 +0200

    fix: geolocation

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -1,9 +1,8 @@
 <template>
+  <div class="weather-tile" v-if="!position">
+    <button @click="getLocation" >Refresh</button>
+  </div>
   <div class="weather-tile" v-if="weather">
-    <div class="datetime">
-      <div>{{ date }}</div>
-      <div>{{ time }}</div>
-    </div>
     <div class="today">
       <img :src="weather.condition.icon">
       <div>{{ weather.condition.text }}</div>
@@ -42,143 +41,112 @@
 import { onMounted, ref, type Ref } from 'vue'
 import axios from 'axios'
 
-export interface Weather {
-  lat: number;
-  lon: number;
-  timezone: string;
-  timezone_offset: number;
-  current: Current;
-  minutely?: (MinutelyEntity)[] | null;
-  hourly?: (HourlyEntity)[] | null;
-  daily?: (DailyEntity)[] | null;
-  alerts?: (AlertsEntity)[] | null;
-}
-export interface Current {
-  dt: number;
-  sunrise: number;
-  sunset: number;
-  temp: number;
-  feels_like: number;
-  pressure: number;
-  humidity: number;
-  dew_point: number;
-  uvi: number;
-  clouds: number;
-  visibility: number;
-  wind_speed: number;
-  wind_deg: number;
-  wind_gust: number;
-  weather?: (WeatherEntity)[] | null;
-}
-export interface WeatherEntity {
-  id: number;
-  main: string;
-  description: string;
-  icon: string;
-}
-export interface MinutelyEntity {
-  dt: number;
-  precipitation: number;
-}
-export interface HourlyEntity {
-  dt: number;
-  temp: number;
-  feels_like: number;
-  pressure: number;
-  humidity: number;
-  dew_point: number;
-  uvi: number;
-  clouds: number;
-  visibility: number;
-  wind_speed: number;
-  wind_deg: number;
-  wind_gust: number;
-  weather?: (WeatherEntity)[] | null;
-  pop: number;
-}
-export interface DailyEntity {
-  dt: number;
-  sunrise: number;
-  sunset: number;
-  moonrise: number;
-  moonset: number;
-  moon_phase: number;
-  summary: string;
-  temp: Temp;
-  feels_like: FeelsLike;
-  pressure: number;
-  humidity: number;
-  dew_point: number;
-  wind_speed: number;
-  wind_deg: number;
-  wind_gust: number;
-  weather?: (WeatherEntity)[] | null;
-  clouds: number;
-  pop: number;
-  rain: number;
-  uvi: number;
-}
-export interface Temp {
-  day: number;
-  min: number;
-  max: number;
-  night: number;
-  eve: number;
-  morn: number;
-}
-export interface FeelsLike {
-  day: number;
-  night: number;
-  eve: number;
-  morn: number;
-}
-export interface AlertsEntity {
-  sender_name: string;
-  event: string;
-  start: number;
-  end: number;
-  description: string;
-  tags?: (null)[] | null;
+export interface WeatherReply {
+  location: Location
+  current: Current
 }
 
+export interface Location {
+  name: string
+  region: string
+  country: string
+  lat: number
+  lon: number
+  tz_id: string
+  localtime_epoch: number
+  localtime: string
+}
+
+export interface Current {
+  last_updated_epoch: number
+  last_updated: string
+  temp_c: number
+  temp_f: number
+  is_day: number
+  condition: Condition
+  wind_mph: number
+  wind_kph: number
+  wind_degree: number
+  wind_dir: string
+  pressure_mb: number
+  pressure_in: number
+  precip_mm: number
+  precip_in: number
+  humidity: number
+  cloud: number
+  feelslike_c: number
+  feelslike_f: number
+  vis_km: number
+  vis_miles: number
+  uv: number
+  gust_mph: number
+  gust_kph: number
+}
+
+export interface Condition {
+  text: string
+  icon: string
+  code: number
+}
+
+
 const weatherApiKey = ref("9997f78ebf6b4f99a15190339200605")
-const date = ref('')
-const time = ref('')
 const position: Ref<GeolocationPosition | null> = ref(null)
-const weather: Ref<Weather | null> = ref(null)
+const weather: Ref<Current | null> = ref(null)
 
 
 async function getWeather() {
-  //https://www.weatherapi.com/docs/
-  axios
-    .get('https://api.weatherapi.com/v1/current.json', {
-      params: {
-        key: weatherApiKey.value,
-        q: `${position.value?.coords.latitude},${position.value?.coords.longitude}`
-      }
-    })
-    .then(response => {
-      weather.value = response.data.current
-    })
-    .catch(error => {
-      console.error('getWeather', error);
-    });
-}
+  if(!position.value) return false
 
-function getLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(p => {
-      position.value = p;
-      getWeather()
-    });
+  try {
+  
+    //https://www.weatherapi.com/docs/
+
+    console.log('Getting weather data...')
+    const response = await axios
+      .get('https://api.weatherapi.com/v1/currentv.json', {
+        params: {
+          key: weatherApiKey.value,
+          q: `${position.value?.coords.latitude},${position.value?.coords.longitude}`
+        }
+      })
+
+    console.log(response.data)
+
+    weather.value = response.data.current
+
+  } catch (error) {
+    console.error(error)
   }
 }
 
+function getLocation() {
+  navigator.geolocation.getCurrentPosition(p => {
+    console.log(p)
+    position.value = p;
+    getWeather()
+  },
+  (e) => console.error(e),
+  { enableHighAccuracy: true }
+);
+}
 
-onMounted(() => {
-  getLocation();
-})
 
+
+// setInterval(() => {
+//   getWeather()
+// }, 60*1000)
 
 
 </script>
+
+
+<style lang="css" scoped>
+
+
+.weather-tile
+{
+
+}
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,49 @@
 <script setup lang="ts">
-</script>
+import Weather from '@/components/Weather.vue'
+import { ref } from 'vue'
+import { RouterView } from 'vue-router'
 
+
+const date = ref(new Date())
+
+function refresh() {
+  date.value = new Date()
+}
+
+
+setInterval(() => {
+  refresh()
+}, 2000)
+
+</script>
 <template>
+  <header>
+
+    <div>
+      <h1>{{ date.toLocaleDateString('fr-CH', { weekday: "long", month: "long", day: "numeric" }) }}</h1>
+      <h2>{{ date.getHours() }}:{{ date.getMinutes() }}</h2>
+      <p>St Romain</p>
+    </div>
+    <div>
+      <Weather />
+    </div>
+
+  </header>
+
   <main>
 
     Home
 
   </main>
 </template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  display: flex;
+  place-items: flex-start;
+  flex-direction: row;
+  width: 100%;
+  justify-content: space-between;
+}
+</style>
